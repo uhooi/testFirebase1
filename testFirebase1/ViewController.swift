5e6f7a71661fe04020a92a7bee535bf45034bfe7
MHTmikiya <m.okugawa@mht-code.com>
2019-07-09 21:17:58 +0900

    Initial Commit

---
+++ testFirebase1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testFirebase1
+//
+//  Created by がーたろ on 2019/07/09.
+//  Copyright © 2019 がーたろ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
