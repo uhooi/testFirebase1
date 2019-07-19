b6b043e2b3ffd91ba30d881be7b25700d8837725
MHTmikiya <m.okugawa@mht-code.com>
2019-07-20 02:47:03 +0900

    セミナー用いろんなもん消した

--- testFirebase1/ChatRoomVIewController.swift
+++ testFirebase1/ChatRoomVIewController.swift
@@ -12,25 +12,23 @@ import FirebaseDatabase
 import RxCocoa
 import RxSwift
 
-
-
 class ChatRoomViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var sendButton: UIButton!
     @IBOutlet var messageTextField: UITextField!
     @IBOutlet var tableView: UITableView!
     var databaseRef:DatabaseReference!
-    var messages = [Any]()
     private let disposeBag = DisposeBag()
-    
+    // RealtimeDatabaseに送るメッセージデータ
+    var messages = [Any]()
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
     }
 
     func initView() {
-        changeLoginEnabled()
-        self.sendButton.layer.cornerRadius = 25
+        sendButton.addTarget(self, action: #selector(self.sendData), for: .touchUpInside)
+        sendButton.layer.cornerRadius = 25
         self.messageTextField.layer.cornerRadius = 5
         self.title = "TUB"
         self.navigationController?.navigationBar.titleTextAttributes = [
@@ -41,49 +39,28 @@ class ChatRoomViewController: UIViewController, UITableViewDelegate, UITableView
         self.navigationController?.navigationBar.tintColor = .white
         self.navigationItem.hidesBackButton = true
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "退出", style: .plain, target: self, action: #selector(exit(_:)))
-        sendButton.addTarget(self, action: #selector(self.sendData), for: .touchUpInside)
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.showsVerticalScrollIndicator = true
         tableView.indicatorStyle = UIScrollView.IndicatorStyle.black
         tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
-        tableView.transform = __CGAffineTransformMake(1, 0, 0, -1, 0, 0)
-        databaseRef = Database.database().reference()
-        databaseRef.observe(.childAdded, with: { snapshot in
-            if let obj = snapshot.value as? [String : AnyObject], let name = obj["name"] as? String, let message = obj["message"] {
-                self.messages.insert(obj, at: 0)
-                self.tableView.reloadData()
-            }
-        })
-        // バリデーション
-        validTxtField(textField: messageTextField)
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         tableView.separatorStyle = .none
-        sendButton.isEnabled = false
     }
     
     @objc func sendData() {
-        if let message = messageTextField.text {
-            let messageData = ["name": UserDefaults.standard.string(forKey: "name"), "message": message]
-            databaseRef.childByAutoId().setValue(messageData)
-            messageTextField.text = ""
-            changeLoginEnabled()
-        }
+        // データ書き込み機能
+
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        
         return true
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messages.count
@@ -91,75 +68,17 @@ class ChatRoomViewController: UIViewController, UITableViewDelegate, UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as? TableViewCell
-        let dictionary = messages[indexPath.row] as! [String: AnyObject]
-        cell?.messageLabel?.text = dictionary["message"] as? String
-        cell?.nameLabel?.text = dictionary["name"] as? String
-        if cell?.nameLabel?.text == UserDefaults.standard.string(forKey: "name") {
-            cell?.messageLabel.backgroundColor = UIColor(hex: "D7003E")
-            cell?.messageLabel.textColor = UIColor(hex: "FFFFFF")
-            cell?.iconImg.image = UIImage(named: "TUB_Red")
-        }
-        cell!.transform = __CGAffineTransformMake(1, 0, 0, -1, 0, 0)
-        
         
         return cell!
     }
     
-
-    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 76
     }
-    
-    //退出時のアラート
-    func showNotAnswerDialog() {
-        let alert: UIAlertController = UIAlertController(title: "退出", message: "現在のルームから退出しますか？", preferredStyle: .alert)
-        // 決定ボタン
-        let defaultAction: UIAlertAction = UIAlertAction(title: "退出", style: .default, handler: { (_: UIAlertAction!) -> Void in
-            self.navigationController?.popViewController(animated: true)
-        })
-        // キャンセルボタン
-        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: { (_: UIAlertAction!) -> Void in
-        })
-        alert.addAction(cancelAction)
-        alert.addAction(defaultAction)
-        alert.preferredAction = defaultAction
-        present(alert, animated: true, completion: nil)
-        
-    }
 
     @objc func exit(_ sender: UIBarButtonItem) {
-        showNotAnswerDialog()
+        self.navigationController?.popViewController(animated: true)
     }
-    
-    // 文字数チェック
-    private func changeLoginEnabled() {
-        if messageTextField.text!.count > 0{
-            // ボタンの活性状態
-            sendButton.isEnabled = true
-            sendButton.backgroundColor = UIColor(hex: "D7003E")
-            sendButton.setTitleColor(UIColor.white, for: .normal)
-        } else {
-            sendButton.isEnabled = false
-            sendButton.backgroundColor = UIColor(hex: "801a2e")
-            sendButton.setTitleColor(UIColor.gray, for: .normal)
-        }
-        
-    }
-    
-    func validTxtField(textField: UITextField) {
-        // textの変更を検知する
-        textField.rx.text.subscribe(onNext: { _ in
-            print(textField.text!.count)
-            
-            // チェック関数の呼び出し
-            self.changeLoginEnabled()
-        }).disposed(by: disposeBag)
-    }
-    
-
-
-
 }
 
 
--- testFirebase1/UserRegistViewController.swift
+++ testFirebase1/UserRegistViewController.swift
@@ -15,16 +15,14 @@ class UserRegistViewController: UIViewController {
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var sendButton: UIButton!
     private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        self.nameTextField.text = ""
-    }
-    
     func initView() {
+        // 見た目
         self.nameTextField.layer.cornerRadius = 5
         self.sendButton.layer.cornerRadius = 25
         self.title = "TUB"
@@ -35,37 +33,19 @@ class UserRegistViewController: UIViewController {
         ]
         self.navigationController?.navigationBar.tintColor = .white
         sendButton.addTarget(self, action: #selector(self.moveToChat), for: .touchUpInside)
-        validTxtField(textField: nameTextField)
+    }
+    
+    // チャット画面から戻ってきた時にろユーザー情報を解錠する
+    override func viewWillAppear(_ animated: Bool) {
     }
     
     @objc func moveToChat() {
-        UserDefaults.standard.removeObject(forKey: "name")
-        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        // UserDefaultを使ってチャット画面にログイン名を登録する
+        
         let storyboard: UIStoryboard = UIStoryboard(name: "ChatRoom",bundle: nil)
         let viewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "ChatRoom")
         show(viewController, sender: nil)
     }
     
-    private func changeLoginEnabled() {
-        if nameTextField.text!.count > 0{
-            // ボタンの活性状態
-            sendButton.isEnabled = true
-            sendButton.backgroundColor = UIColor(hex: "D7003E")
-            sendButton.setTitleColor(UIColor.white, for: .normal)
-        } else {
-            sendButton.isEnabled = false
-            sendButton.backgroundColor = UIColor(hex: "801a2e")
-            sendButton.setTitleColor(UIColor.gray, for: .normal)
 
-        }
-    }
-
-    func validTxtField(textField: UITextField) {
-        // textの変更を検知する
-        textField.rx.text.subscribe(onNext: { _ in
-            print(textField.text!.count)
-            // チェック関数の呼び出し
-            self.changeLoginEnabled()
-        }).disposed(by: disposeBag)
-    }
 }
